aefb97a90963b7b11832ee112c28c552bf72a7da
juanmqc22 <juanmqc22@gmail.com>
2023-03-15 23:17:30 -0300

    changes

--- instrutores/suplente/suplente.js
+++ instrutores/suplente/suplente.js
@@ -122,22 +122,74 @@ campoBusca.addEventListener('keyup', () => {
     });
 });
 
-function enviarPorEmail() {
-    // Obtem o corpo do e-mail a partir da tabela de dados
-    const tabela = document.getElementById('name-table');
-    const corpoEmail = tabela.outerHTML;
-  
-    // Cria um link com a URL de um cliente de e-mail com o corpo do e-mail preenchido
-    const link = document.createElement('a');
-    link.href = `mailto:?subject=Dados da tabela&body=${encodeURIComponent(corpoEmail)}`;
-  
-    // Simula um clique no link para abrir o cliente de e-mail padrão do usuário
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    console.log(tabela)
-  }
-  
-  // Adiciona o evento de clique ao botão "Enviar por e-mail"
-  document.getElementById('btn-email').addEventListener('click', enviarPorEmail);
-  
+// function enviarPorEmail() {
+//     // Obtem o corpo do e-mail a partir da tabela de dados
+//     const tabela = document.getElementById('name-table');
+//     const corpoEmail = tabela.outerHTML;
+
+//     // Cria um link com a URL de um cliente de e-mail com o corpo do e-mail preenchido
+//     const link = document.createElement('a');
+//     link.href = `mailto:?subject=Dados da tabela&body=${encodeURIComponent(corpoEmail)}`;
+
+//     // Simula um clique no link para abrir o cliente de e-mail padrão do usuário
+//     document.body.appendChild(link);
+//     link.click();
+//     document.body.removeChild(link);
+//     console.log(tabela)
+// }
+
+// Adiciona o evento de clique ao botão "Enviar por e-mail"
+// document.getElementById('btn-email').addEventListener('click', enviarPorEmail);
+
+$(document).ready(function () {
+    $('#btn-email').click(function () {
+        var tableData = getTableData();
+        tableData.forEach(rowData => {
+            console.log(rowData.name + ": " + rowData.checkboxes.join(", "));
+        });
+    });
+});
+
+function getTableData() {
+    var tableData = [];
+    $('#name-table tr').each(function (row, tr) {
+        if (row !== -1) { // ignore the table header row
+            var rowData = {
+                name: "",
+                checkboxes: [],
+                textInput: ""
+            };
+            $(tr).find('td').each(function (col, td) {
+                if (col === 0) {
+                    rowData.name = td.textContent;
+                } else if (col >= 1 && col <= 5) {
+                    if (col === 1) {
+                        rowData.checkboxes.push($(td).find('input[type="checkbox"]').prop('checked') ? false : "Faltou");
+                    } else {
+                        const isChecked = $(td).find('input[type="checkbox"]').prop('checked');
+                        rowData.checkboxes.push(col === 1 && !isChecked ? "atraso" : isChecked);
+                    }
+                } else if (col === 6) {
+                    rowData.textInput = $(td).find('input[type="text"]').val();
+                }
+            });
+            if (rowData.checkboxes.includes(true)) {
+                tableData.push(rowData);
+            }
+        }
+    });
+    return tableData;
+}
+
+
+
+
+// function areCheckboxesChecked() {
+//     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+//     checkboxes.forEach((checkbox) => {
+//         if (checkbox.checked) {
+//             console.log(checkbox.parentNode.parentNode.firstChild.textContent + " is checked");
+//         } else {
+//         }
+//     });
+// }
